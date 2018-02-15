28211b9d11eeac874afdfbc5c075659d2d4343de
Unknown <yeisoncifuentes2@gmail.com>
2018-02-15 16:57:53 -0500

    Area Circulo
    Halla el area del circulo apartir del radio

---
+++ Main.java
@@ -0,0 +1,22 @@
+public class Main{
+
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("clase de programacion II");
+		System.out.println("EJERCICIO AREA DE FIGURAS");
+		System.out.println("");
+		System.out.println("\n\n\t\tEjercicio N 1");
+		System.out.println("");
+		
+		System.out.println("AREA CIRCULO");
+		double radio = 6;
+		double pi = 3.1415;
+		System.out.println("Con Radio ="+radio);
+		System.out.println("El area del circulo es:"+(pi*(Math.pow(radio,2)))+" cm^2");
+		System.out.println("");
+		
+               
+		
+		
+	}
+}
